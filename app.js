ee33caed36bffdfbc65dad76fa0a21a6dd9eb6d3
Strider <fernando.herrera85@gmail.com>
2017-06-02 16:41:36 -0600

    Se realizo un reset

---
+++ app.js
@@ -0,0 +1,17 @@
+/*
+  Autor: Fernando Herrera
+  Descripcion: Este es el archivo principal de mi aplicación
+  Creado en: 01-01-2030
+*/
+
+
+
+function unir_nombre(nombres){
+  // Aqui esperamos un arreglo
+  return nombres.join(" ");
+
+}
+
+function separar_nombres(){
+
+}
